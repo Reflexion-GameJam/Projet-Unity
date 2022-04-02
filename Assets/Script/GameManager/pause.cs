8ea413a7c45ed49964c9e41f06029df7e608cda5
Maxime66410 <max66410@hotmail.fr>
2022-04-02 14:47:19 +0200

    Pause Script

---
+++ Assets/Script/GameManager/pause.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    public int pauseInt;
+
+    private void Start()
+    {
+        // Si le menu de pause n'est pas assigné
+        if (pausePanel == null)
+        {
+            pausePanel = GameObject.Find("PauseSystem");
+        }
+        
+        pausePanel.SetActive(false);
+
+        pauseInt = 0;
+    }
+
+    private void Update()
+    {
+        // Si le joueur appuie sur "Echap"
+        if (Input.GetKeyUp(KeyCode.Escape) || Input.GetButtonUp("Cancel"))
+        {
+            // Si le panel est désactiver
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }   // Si activer
+            else if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+
+        // Déblocage de la souris
+        if (pauseInt == 1)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Mise en pause
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        pauseInt = 1;
+    }
+
+    // Mise en marche
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        pauseInt = 0;
+    }
+}
