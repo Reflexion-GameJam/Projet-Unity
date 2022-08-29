5b08db6ef66b594c342821ea1f4f635b5eed733f
Maxime66410 <max66410@hotmail.fr>
2022-08-29 12:24:05 +0200

    Add Credit Page

---
+++ Assets/Scripts/UI/Menu/Credit.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    public void OpenLinkPlayCurious()
+    {
+        Application.OpenURL("https://playcurious.games/fr/");
+    }
+    
+    public void OpenLinkGameJam()
+    {
+        Application.OpenURL("https://playcurious.games/fr/jam22/");
+    }
+    
+    public void OpenLinkLudi()
+    {
+        Application.OpenURL("https://www.ludiconcept.fr");
+    }
+    
+    public void OpenLinkMax()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/maxime-sanchez-98837015a/");
+    }
+    
+    public void OpenLinkAspireen()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/alexis-vast/");
+    }
+    
+    public void OpenLinkWolfcat()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/julia-barrois-829041237/");
+    }
+    
+    public void OpenLinkDeorum()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/paul-moussy/");
+    }
+    
+    public void OpenLinkCoops()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/florian-dhieux-920746237/");
+    }
+}
